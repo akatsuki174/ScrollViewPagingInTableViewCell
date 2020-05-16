15878cd0d9863efef7d241b2ee9784b50cbf761c
akatsuki174 <dev2015js@gmail.com>
2020-05-16 21:11:41 +0900

    refactor variable

--- ScrollViewPagingInTableViewCell/PageScrollCell.swift
+++ ScrollViewPagingInTableViewCell/PageScrollCell.swift
@@ -30,22 +30,20 @@ class PageScrollCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
     func setContent(images: [UIImage]) {
-        let screenWidth = UIScreen.main.bounds.width
-        scrollView.frame = CGRect(x: 0, y: 0, width: screenWidth, height: screenWidth * 0.5)
-        scrollView.backgroundColor = UIColor.gray
+        let contentWidth = UIScreen.main.bounds.width
+        let contentHeight = contentWidth * 0.5
+        scrollView.frame = CGRect(x: 0, y: 0, width: contentWidth, height: contentHeight)
         containerView.addSubview(scrollView)
         
         for (index, value) in images.enumerated() {
-            let imageView = UIImageView(frame: CGRect(x: CGFloat(index) * screenWidth, y: 0, width: screenWidth, height: screenWidth * 0.5))
+            let imageView = UIImageView(frame: CGRect(x: CGFloat(index) * contentWidth, y: 0, width: contentWidth, height: contentHeight))
             imageView.image = value
             scrollView.addSubview(imageView)
         }
-        scrollView.contentSize = CGSize(width: screenWidth * CGFloat(images.count), height: screenWidth * 0.5)
+        scrollView.contentSize = CGSize(width: contentWidth * CGFloat(images.count), height: contentHeight)
     }
     
 }
