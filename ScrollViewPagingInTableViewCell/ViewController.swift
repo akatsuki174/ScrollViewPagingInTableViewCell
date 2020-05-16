e3000c599edd272cf2e2d8cebb21e4074b2c8aa1
akatsuki174 <dev2015js@gmail.com>
2020-05-16 16:44:27 +0900

    basic tableview implement

--- ScrollViewPagingInTableViewCell/ViewController.swift
+++ ScrollViewPagingInTableViewCell/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "hoge"
+        return cell
+    }
+        
+}
